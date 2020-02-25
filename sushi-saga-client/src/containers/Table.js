0f7362f54d69ec8aa10e0888bfd18df9d511d646
github username <github email address>
2020-02-25 14:55:57 -0800

    Done.

--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,32 +1,54 @@
-import React, { Fragment } from 'react'
+import React, { Component } from 'react'
 
-const Table = (props) => {
+class Table extends Component {
+  
+  state = {
+    money_input: 0
+  }
 
-  const renderPlates = (array) => {
+  renderPlates = (array) => {
     return array.map((x, index) => {
       return <div className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
 
-  return (
-    <Fragment>
-      <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
-      </h1>
-      <div className="table">
-        <div className="stack">
-          {
-            /* 
-               renderPlates takes an array 
-               and renders an empty plate
-               for every element in the array
-            */
-            renderPlates([])
-          }
+  handleChange = (e) => {
+    console.log("CHANGED THE MONEY", e.target.value)
+    this.setState({
+      money_input: e.target.value
+    })
+
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <h1 className="remaining">
+          You have: ${ this.props.money } remaining!
+
+        <form className="money-form" onSubmit={(e) => this.props.addMoney(e, this.state.money_input)}> 
+          <label>
+          Add Money:
+          </label>
+          <input type="number" name="money" onChange={(e) => this.handleChange(e)}/> 
+          <input type="submit" value="Submit" />
+        </form>
+        </h1>
+
+        <div className="table">
+          <div className="stack">
+            {
+              /* 
+                renderPlates takes an array 
+                and renders an empty plate
+                for every element in the array
+              */
+              this.renderPlates(this.props.eaten)
+            }
+          </div>
         </div>
-      </div>
-    </Fragment>
-  )
+      </React.Fragment>
+    );
+  }
 }
 
 export default Table
